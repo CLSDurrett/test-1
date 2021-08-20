87843c16242ca5b538577617824b686ba31df585
AdamDurrett <89200207+CLSDurrett@users.noreply.github.com>
2021-08-20 12:32:14 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    int x = 0;
+		int y = 0;
+		for(int i=0; i<20; i++){
+			x++;
+
+			if((x==4)||(x==6)||(x==8)){
+				System.out.println("goteem");
+			}
+			if(x<12 && x>3){
+				y+=5;
+				System.out.println(y);
+			}
+		}
+  }
+}
